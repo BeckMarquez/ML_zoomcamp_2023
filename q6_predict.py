e78009b753c856b97838ec4a133704e5f13073c2
Vitaly_U <uvvdiam@gmail.com>
2023-10-10 21:15:47 +0300

    ML ZOOMCAMP Week 5 all 6 questions answered

---
+++ q6_predict.py
@@ -0,0 +1,27 @@
+import pickle
+from flask import Flask
+from flask import request
+from flask import jsonify
+
+def load(filename: str):
+    with open(filename, 'rb') as f_n:
+        return pickle.load(f_n)
+
+dv = load('dv.bin')
+model = load('model2.bin')
+
+app = Flask('Credit score')
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    client = request.get_json()
+
+    X = dv.transform([client])
+    y_pred = model.predict_proba(X)[0, 1]
+
+    score = float(y_pred)
+
+    return jsonify(score)
+
+if __name__ == "__main__":
+    app.run(debug=True, host='0.0.0.0', port=9696)
